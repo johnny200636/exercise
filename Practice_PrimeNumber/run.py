d9de714823601ababd60a3f5115246a57cbffa64
johnny200636 <johnny200636@gmail.com>
2015-11-19 17:29:22 +0800

    modified some problems which lee raised

--- Practice_PrimeNumber/run.py
+++ Practice_PrimeNumber/run.py
@@ -1,24 +1,19 @@
 import math
 
-input_number = input('Please enter a integer:')
-List_prime_number = [1]
+input_number =  input('Please enter a integer : ')
+while float(input_number).is_integer() == False:
+    input_number =  input('Please enter a integer : ')
 
-if float(input_number).is_integer() == False:
-    input_number =  input('Input a integer, you dumbass == :')
-if input_number == 1:
-    print 1
-    
+List_prime_number = [1]
 for x in range(2,input_number+1):
     determinant = 0
     for y in range(0,len(List_prime_number)):
-        if List_prime_number[y] > int(math.sqrt(x)//1):
-            break
-        if (float(x)/float(List_prime_number[y])).is_integer() == True:
+        if determinant == 2: break
+        if x%List_prime_number[y] == 0:
             determinant = determinant + 1
     if determinant == 1:
         List_prime_number.append(x)
-List_prime_number.remove(1)
-print List_prime_number
+        print x,
+print ('')
+del List_prime_number[:]
 
-
-    
